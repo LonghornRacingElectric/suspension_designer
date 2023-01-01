6588a6efa96799375310d1abf730eb0ea6d1f249
bechrist <40005251+bechrist@users.noreply.github.com>
2022-12-31 23:16:54 -0800

    instant centers

--- suspension_designer/kinematics.py
+++ suspension_designer/kinematics.py
@@ -1,69 +1,69 @@
 """kinematics.py - Kinematic Frames, Transforms, and Systems"""
-
 from __future__ import annotations
 
-import collections as col
-import numpy.typing as npt
+from collections import UserDict
+from numpy.typing import ArrayLike
 
 import numpy as np
 import networkx as nx
+import matplotlib.pyplot as plt
 
-
-__all__ = ['poi_factory', 'euler_rotation', 
+__all__ = ['poi_factory', 'default_poi_factory', 'euler_rotation', 
            'KinematicFrame', 'KinematicTransform', 'KinematicSystem']
 
 
-def poi_factory(name: str, position: npt.ArrayLike, style: str = 'k.' ) -> dict:
+def poi_factory(name: str, position: ArrayLike, style: str = 'k.' ) -> dict:
     """Point of Interest Factory"""
-    return {name: {'position': np.array(position, dtype=np.float), 'style': style}}
+    return {name: {'position': np.array(position, dtype=np.double), 'style': style}}
 
 
 def default_poi_factory() -> dict:
     """Default Points of Interest Factory"""
     out = {}
-    for poi in (['O', [0,0,0]], ['E1', [1,0,0]], ['E2', [0,1,0]], ['E3', [0,0,1]]):
+    for poi in (['O', [0,0,0]], ['E0', [1,0,0]], ['E1', [0,1,0]], ['E2', [0,0,1]]):
         out.update(poi_factory(*poi)) 
 
     return out 
 
 
-def euler_rotation(angle: float, axis: int, point: npt.ArrayLike) -> npt.ArrayLike:
+def euler_rotation(angle: float, axis: int, point: ArrayLike) -> ArrayLike:
     """Applies a rotation transform about a primary frame axis"""
-    ia: int = np.mod(axis,3)
+    ia  : int = np.mod(axis,3)
     iap1: int = np.mod(axis+1,3)
     iam1: int = np.mod(axis-1,3)
 
-    R = np.zeros((3,3))
+    R = np.zeros((3,3), dtype=np.double)
     R[ia  , ia  ] = 1
-    R[iam1, iam1] = np.cos(angle)
-    R[iap1, iap1] = np.cos(angle)
+    R[iam1, iam1] =  np.cos(angle)
+    R[iap1, iap1] =  np.cos(angle)
     R[iam1, iap1] =  np.sin(angle)
     R[iap1, iam1] = -np.sin(angle)
 
     return np.matmul(R, point)
 
 
-class KinematicFrame(col.UserDict):
+class KinematicFrame(UserDict):
     """Kinematic reference frame in 3D space"""
     def __init__(self, name: str='', poi: dict=default_poi_factory(), style: str='k'):
         """Allocate KinematicFrame instance properties"""
         super().__init__({'name': name, 'poi': poi, 'style': style})
 
-
-class KinematicTransform(col.UserDict):
+class KinematicTransform(UserDict):
     """Kinematic tranformation between reference frames"""
     def __init__(self, 
-            position: npt.ArrayLike=np.empty(3),
-            rotation: npt.ArrayLike=np.empty(3),
-            sequence: npt.ArrayLike=np.array([0,3,2,1]) ):
+            position: ArrayLike=np.zeros(3, dtype=np.double),
+            rotation: ArrayLike=np.zeros(3, dtype=np.double),
+            sequence: ArrayLike=np.array([0,3,2,1], dtype=np.ushort),
+            dof     : ArrayLike=np.zeros(6, dtype=np.bool_) ):
         """Allocate KinematicTransform instance properties"""
         super().__init__({
-            'position': np.array(position), 
-            'rotation': np.array(rotation),
-            'sequence': np.array(sequence),
+            'position': np.array(position, dtype=np.double), 
+            'rotation': np.array(rotation, dtype=np.double),
+            'sequence': np.array(sequence, dtype=np.ushort),
+            'dof'     : np.array(dof, dtype=np.bool_),
         })
 
-    def transform(self, point: npt.ArrayLike, direction: str='f' ) -> npt.ArrayLike:
+    def transform(self, point: ArrayLike, direction: str='f' ) -> ArrayLike:
         """Streamline invoking kinematic affine transformations"""
         if any([direction == d for d in ['f', 'forward']]):
             return self.forward_transform(np.array(point))
@@ -72,25 +72,25 @@ class KinematicTransform(col.UserDict):
         else:
             raise ValueError('Transformation direction argument not valid') 
 
-    def forward_transform(self, pB: npt.ArrayLike) -> npt.ArrayLike:
+    def forward_transform(self, pB: ArrayLike) -> ArrayLike:
         """Perform forward transform from base to follower frame"""
         pF = pB
-        for s in self.sequence:
+        for s in self['sequence']:
             if s == 0:
-                pF = pF - self.position
+                pF = pF - self['position']
             else:
-                pF = euler_rotation(self.rotation[s-1], s-1, pF) 
+                pF = euler_rotation(self['rotation'][s-1], s-1, pF) 
 
         return pF 
             
-    def inverse_transform(self, pF: npt.ArrayLike) -> npt.ArrayLike:
+    def inverse_transform(self, pF: ArrayLike) -> ArrayLike:
         """Perform inverse transform from follower to base frame"""
         pB = pF
-        for s in np.flip(self.sequence):
+        for s in np.flip(self['sequence']):
             if s == 0:
-                pB = pB + self.position
+                pB = pB + self['position']
             else:
-                pB = euler_rotation(-self.rotation[s-1], s-1, pB)
+                pB = euler_rotation(-self['rotation'][s-1], s-1, pB)
 
         return pB 
 
@@ -104,15 +104,63 @@ class KinematicSystem(nx.DiGraph):
         super().__init__(incoming_graph_data, **attr)
         self.cycle = []
 
-    def get_cycles(self, source=None, subgraph=None):
+    def compute_weights(self):
+        for e in self.edges():
+            self.edges[e]['weight'] = 10+np.sum(self.edges[e]['dof'])
+
+    def get_path(self, source: str, target: str) -> list[tuple[str,str,str]]:
+        """Generates transform sequence between two coordinate frames"""
+        nodes = nx.shortest_path(self.to_undirected(as_view=True), source, target)
+
+        path = []
+        for j in range(len(nodes)-1):
+            if (nodes[j], nodes[j+1]) in self.edges:
+                path.append((nodes[j], nodes[j+1], 'forward'))
+            elif (nodes[j+1], nodes[j]) in self.edges:
+                path.append((nodes[j+1], nodes[j], 'reverse'))
+            else:
+                KeyError("Edge ({},{}) is not present".format(nodes[j], nodes[j+1]))
+
+        return path
+
+    def get_loops(self, subgraph=None):
+        """Generates a directed minimum loop basis for a subgraph"""
         if subgraph is None: 
             goi = self
         else:
             goi = self.subgraph(subgraph)
 
-        cycle = []
-        for c in nx.minimum_cycle_basis(goi.to_undirected(as_view=True)):
-            cycle.append( nx.find_cycle(
-                goi.subgraph(c), source=source, orientation='ignore') )
+        mcb = nx.minimum_cycle_basis(goi.to_undirected(as_view=True), weight='weight')
 
-        return cycle
+        self.loop = []
+        for c in mcb:
+            self.loop.append(nx.find_cycle(goi.subgraph(c), orientation='ignore'))
+
+    def coord(self, point: str | ArrayLike, frame: str, outFrame: str) -> ArrayLike:
+        """Reports point of interest coordinates in requested frame"""
+        # Fetch point
+        if isinstance(point, str):
+            point = self.nodes[frame]['poi'][point]['position']
+
+        for (frameA, frameB, direction) in self.get_path(frame, outFrame):
+            point = self.edges[frameA, frameB].transform(point, direction)
+
+        return point
+
+    def plot(self, ax: plt.axes=None, frame: str=None, size: int=1):
+        """Plots KinematicSystem"""
+        # Default parameters
+        ax = plt.gca() if ax is None else ax
+        frame = list(self.nodes)[0] if frame is None else frame
+
+        # Plot Cartesian frames
+        for node in self.nodes():
+            O = self.coord('O', node, frame)
+            for i in range(3):
+                ax.quiver3D(*O, *(self.coord('E{}'.format(i), node, frame)-O), 
+                    color='k', length=size, normalize=True)
+
+        ax.set_xlabel('X [mm]')
+        ax.set_ylabel('Y [mm]')
+        ax.set_zlabel('Z [mm]')
+        ax.set_aspect('equal')
--- suspension_designer/linkages/double_wishbone.py
+++ suspension_designer/linkages/double_wishbone.py
@@ -1,28 +1,147 @@
 """double_wishbone.py - Double Wishbone Linkage"""
+from typing import Callable
+from numpy.typing import ArrayLike
+
+import numpy as np;
+from scipy.optimize import fsolve;
+
+import matplotlib.pyplot as plt
 
 import os, sys; 
-
 sys.path.insert(0, os.path.realpath(''))
-from suspension_designer.kinematics import KinematicSystem
 
+from suspension_designer.kinematics import poi_factory, KinematicSystem
 
 class DoubleWishbone(KinematicSystem):
     def __init__(self, options: dict, **kwargs):
         # Network Generation
-        super().__init__()
+        super().__init__(**kwargs)
 
         self.add_edges_from([
-            ('E', 'I'), 
             ('I','T'), ('T','W'),
             ('I','B'), ('B','X'),
             ('X','LA'), ('X','UA'), ('X','TA'),
             ('LA','LB'), ('UA','UB'), ('TA','TB'),
-            ('LB','W'), ('UB','W'), ('TB','W')])
+            ('W','LB'), ('W','UB'), ('W','TB')])
 
+        # Degrees of Freedom
+        self.edges[('I','T')]['dof'][:] = [1,1,0,1,0,1]
+        self.edges[('T','W')]['dof'][:] = [0,0,0,0,1,0]
+
+        self.edges[('X','LA')]['dof'][:] = [0,0,0,1,0,0]
+        self.edges[('X','UA')]['dof'][:] = [0,0,0,1,0,0]
+        self.edges[('X','TA')]['dof'][:] = [0,0,0,1,0,1]
+
+        self.edges[('W','LB')]['dof'][:] = [0,0,0,1,1,1]
+        self.edges[('W','UB')]['dof'][:] = [0,0,0,1,1,1]
+        self.edges[('W','TB')]['dof'][:] = [0,0,0,1,1,1]
+
+        self.compute_weights()
+        
         # Allocate Options
         self.vehicle = options['vehicle']
         self.target  = options['target']
         self.bound   = options['bound']
 
-    def design(self):
-        pass
+        # Compute Additional Targets
+        self.target['FVSA'] = 1/np.tan(np.deg2rad(np.abs(self.target['camber_gain'])))
+        self.target['SVSA'] = 1/np.tan(np.deg2rad(np.abs(self.target['caster_gain'])))
+
+    def design(self, design: ArrayLike=np.random.rand(9)):
+        """Generate design meeting targets and bounds"""
+        ### Sample Design ###
+        lerp: Callable[[float,float,float],float] = lambda x0, x1, t: x0*(1-t) + x1*t
+        lerpBound: Callable[[str,int,float],float] = \
+            lambda p, i, t: lerp(self.bound[p][i][0], self.bound[p][i][1], t)
+
+        self.edges['W','LB']['position'][0] = lerpBound('LB', 0, design[0])
+        self.edges['W','LB']['position'][1] = lerpBound('LB', 1, design[1])
+        self.edges['W','LB']['position'][2] = lerpBound('LB', 2, design[2])
+
+        self.edges['W','UB']['position'][0] = lerpBound('UB', 0, design[3])
+        self.edges['W','UB']['position'][2] = lerpBound('UB', 2, design[4])
+
+        ### Apply Vechicle Options and Design Targets ###
+        match self.target['axle']:
+            case "front" | "f":
+                axleSkew = 1-self.vehicle['%f']/100
+            case "rear" | "r":
+                axleSkew = self.vehicle['%f']/100
+            case _:
+                return ValueError("Axle target unrecognized: {}".format(self.target['axle']))
+
+        self.edges['I','T']['position'][0] = self.vehicle['wheelbase']*axleSkew
+        self.edges['I','T']['position'][1] = self.target['track']/2
+        self.edges['I','T']['rotation'][0] = np.deg2rad(self.target['camber'])
+        self.edges['I','T']['rotation'][2] = np.deg2rad(self.target['toe'])
+
+        self.edges['T','W']['position'][2] = self.vehicle['tire_radius']
+        self.edges['T','W']['rotation'][0] = np.deg2rad(self.target['caster'])
+
+        self.edges['I','B']['position'][2] = self.vehicle['cg_height']
+        self.edges['I','B']['rotation'][1] = self.vehicle['rake']*(np.pi/180)
+
+        self.edges['B','X']['position'][0] = self.vehicle['wheelbase']*axleSkew
+        self.edges['B','X']['position'][2] = self.vehicle['ride'] - self.vehicle['cg_height']
+
+        ### Outboard A-Arm Design ###
+        self.edges['W','UB']['position'][1] = self.edges['W','LB']['position'][1] \
+            - np.tan(np.deg2rad(self.target['kpi']))*(self.edges['W','UB']['position'][2] - self.edges['W','LB']['position'][2])
+
+        ### Roll and Pitch Centers ###
+        self.nodes['I']['poi'].update(
+            poi_factory('RC', np.array([self.coord('O','T','I')[0], 0, self.vehicle['cg_height']*(1-self.target['%roll']/100)]), 'kx')
+        )
+
+        self.nodes['I']['poi'].update(
+            poi_factory('PC', np.array([0, self.coord('O','T','I')[1], self.vehicle['cg_height']*(1-self.target['%pitch']/100)]), 'kx')
+        )
+
+        ### Instant Centers ###
+        zRoot = lambda p0, p1, pc, R, z: ((p1[0] - p0[0])/(p1[1] - p0[1]))*(z - p0[1]) + np.sqrt(R**2 - (z-pc[1])**2)
+        jLine = lambda p0, p1, z: p0[0] + ((p1[0] - p0[0])/(p1[1] - p0[1]))*(z - p0[1])
+
+        for (j,m,v) in [[1,'R','F'], [0,'P','S']]:
+            self.nodes['I']['poi'].update(poi_factory(m+'IC', self.coord('O','T','I'), 'ko'))
+
+            if np.isinf(self.target[v+'VSA']):
+                self.nodes['I']['poi'][m+'IC']['position'][[j,2]] = [-np.inf, 0]
+                continue
+
+            self.nodes['I']['poi'][m+'IC']['position'][2] = \
+                fsolve(lambda z: zRoot(self.coord('O','T','I')[[j,2]], self.coord(m+'C','I','I')[[j,2]], 
+                                       self.coord('O','W','I')[[j,2]], self.target[v+'VSA'], z), 0)
+
+            self.nodes['I']['poi'][m+'IC']['position'][j] = \
+                jLine(self.coord('O','T','I')[[j,2]], self.coord(m+'C','I','I')[[j,2]], self.nodes['I']['poi'][m+'IC']['position'][2])
+        
+        
+        fig = plt.figure(figsize=(10,10))
+        ax = plt.axes(projection='3d')
+        self.plot()
+        
+
+        _pause = 1
+
+    def plot(self, ax: plt.axes=None):
+        """Plot DoubleWishbone system"""
+        # Default parameters
+        ax = plt.gca() if ax is None else ax
+
+        # Invoke KinematicSystem plot method
+        super().plot(ax, 'I', 50)
+
+        # Plot kingpin axis
+        KPA = np.vstack((self.coord('O', 'LB', 'I'), self.coord('O', 'UB', 'I')))
+        ax.plot(*KPA.T, 'k--')
+
+        # Plot Centers
+        ax.plot(*self.coord('RC','I','I'), 'kx')
+        ax.plot(*self.coord('PC','I','I'), 'kx')
+
+        ax.plot(*np.vstack([self.coord('O','T','I'), self.coord('RIC','I','I')]).T, 'k:')
+        ax.plot(*np.vstack([self.coord('O','T','I'), self.coord('PIC','I','I')]).T, 'k:')
+
+        # Configure axes
+        ax.view_init(elev=20, azim=30)
+        ax.set_zlim([0, 500])
